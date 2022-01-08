2c9e37a3f33f1f06fd006ab9b47ae310fdb7bdd5
organicToaster <ebanerjee22@gmail.com>
2022-01-08 18:39:44 -0500

    evan's code on getting valid moves for every pawn or rook

--- Chess/chess_machine.py
+++ Chess/chess_machine.py
@@ -55,3 +55,71 @@ class Game_State:
 
         #add to the solution variable all the possible moves if the piece is a rook
         if(piece[1] == "R"):
+
+            #code to determine all spots above the rook that are valid moves
+            keepLooping = True
+            indexVariable = 1
+
+            while(keepLooping):
+                if(row - indexVariable >= 0):
+                    if(self.piece_at_coordinates(self, row - indexVariable, column) == "--"):
+                        solution += [row - indexVariable, column]
+                    elif(self.piece_at_coordinates(self, row - indexVariable, column)[0] == self.piece_at_coordinates(self, row, column)[0]):
+                        keepLooping = False
+                    elif(self.piece_at_coordinates(self, row - indexVariable, column)[0] != self.piece_at_coordinates(self, row, column)[0]):
+                        solution += [row - indexVariable, column]
+                        keepLooping = False
+                else:
+                    keepLooping = False
+                indexVariable += 1
+
+            #code to determine all spots below the rook that are valid moves
+            keepLooping = True
+            indexVariable = 1
+
+            while(keepLooping):
+                if(row + indexVariable <= 7):
+                    if(self.piece_at_coordinates(self, row + indexVariable, column) == "--"):
+                        solution += [row + indexVariable, column]
+                    elif(self.piece_at_coordinates(self, row + indexVariable, column)[0] == self.piece_at_coordinates(self, row, column)[0]):
+                        keepLooping = False
+                    elif(self.piece_at_coordinates(self, row + indexVariable, column)[0] != self.piece_at_coordinates(self, row, column)[0]):
+                        solution += [row + indexVariable, column]
+                        keepLooping = False
+                else:
+                    keepLooping = False
+                indexVariable += 1
+            
+            #code to determine all spots left of the rook that are valid moves
+            keepLooping = True
+            indexVariable = 1
+
+            while(keepLooping):
+                if(column - indexVariable >= 0):
+                    if(self.piece_at_coordinates(self, row, column - indexVariable) == "--"):
+                        solution += [row, column - indexVariable]
+                    elif(self.piece_at_coordinates(self, row, column - indexVariable)[0] == self.piece_at_coordinates(self, row, column)[0]):
+                        keepLooping = False
+                    elif(self.piece_at_coordinates(self, row, column - indexVariable)[0] != self.piece_at_coordinates(self, row, column)[0]):
+                        solution += [row, column - indexVariable]
+                        keepLooping = False
+                else:
+                    keepLooping = False
+                indexVariable += 1
+            
+            #code to determine all spots right of the rook that are valid moves
+            keepLooping = True
+            indexVariable = 1
+
+            while(keepLooping):
+                if(column + indexVariable <= 7):
+                    if(self.piece_at_coordinates(self, row, column + indexVariable) == "--"):
+                        solution += [row, column + indexVariable]
+                    elif(self.piece_at_coordinates(self, row, column + indexVariable)[0] == self.piece_at_coordinates(self, row, column)[0]):
+                        keepLooping = False
+                    elif(self.piece_at_coordinates(self, row, column + indexVariable)[0] != self.piece_at_coordinates(self, row, column)[0]):
+                        solution += [row, column + indexVariable]
+                        keepLooping = False
+                else:
+                    keepLooping = False
+                indexVariable += 1
