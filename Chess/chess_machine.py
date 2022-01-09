f89d88040e7263132ddd64542d7c37bd454cee32
Evan Banerjee <ebanerjee22@gmail.com>
2022-01-09 10:54:32 -0500

    comment time

--- Chess/chess_machine.py
+++ Chess/chess_machine.py
@@ -17,8 +17,8 @@ class Game_State:
         board = [row1, row2, row3, row4, row5, row6, row7, row8]
         self.board = board
 
-        self.is_white_turn = True
-        self.move_log = []
+        self.is_white_turn = True #pretty self explantory
+        self.move_log = [] #this will contain all previous moves when we finally implement that function
     
     def make_move(self, move):  #function that takes in a move object and updates the game_state according to the move made. Assumes move is valid.
         self.board[move.start_row][move.start_col] = "--" #makes the starting location an empty square 
@@ -26,36 +26,81 @@ class Game_State:
         self.move_log.append(move)  #logs the move -- adds it to move log at the end of the log
         self.is_white_turn = not self.is_white_turn #changes turn from white to black or vice versa
 
-
+    def piece_at_coordinates(self, row, column):
+        return self.board[row][column]
 
 
     #return an array of all possible moves for the piece at the inputed square
+    #the array will be 2 dimensional, and each element in the array will be an array of length 2 with the row and column 
+    #of a square that the inputed piece can move to
     def get_valid_moves(self, row, column):
         solution = []
-        piece = self.pieceAtCoordinates(self, row, column)
+        piece = self.piece_at_coordinates(self, row, column)
         
         #add to the solution variable all the possible moves if the piece is a white pawn
         if(piece == "wP"): #checks to see if the selected square has a white pawn
-            if(row > 0 and self.piece_at_coordinates(self, row - 1, column)[0] != "w"): 
+
+            #this basically says: if the white pawn is below the first row (as in it hasn't moved to the end of the board),
+            #and if the piece at the square directly in front of the pawn is empty,
+            #than say that the square directly in front of the pawn is a possible move (by adding that square to the solution array)
+            if(row > 0 and self.piece_at_coordinates(self, row - 1, column) == "--"): 
                 solution += [row - 1, column]
-                if(row == 6 and self.piece_at_coordinates(self, row - 2, column)[0] != "w"):
+
+                #this if statements adds an extra scenario where, if the pawn is on it's starting row, it can move 2 pieces forward
+                #so long as the square 2 pieces forward is empty
+                if(row == 6 and self.piece_at_coordinates(self, row - 2, column) == "--"):
                     solution += [row - 2, column]
+
+            #this collection of if statements adds the possibility of the pawn moving diagonally to the solution array
+            #it says, if the pawn is not on the 0th row,
+            #and if it is not on the farthest column to the left or the farthest column to the right,
             if(row > 0 and (column > 0 and column < 7)):
+
+                #than if the piece one up and one to the left of the selected pawn is black,
                 if(self.piece_at_coordinates(self, row - 1, column - 1)[0] == "b"):
+
+                    #than allow that move to be added to the solution array
                     solution += [row - 1, column - 1]
+
+                #alternatively, if the piece one up and to the right of the selected pawn is black,
                 if(self.piece_at_coordinates(self, row - 1, column + 1)[0] == "b"):
+
+                    #than add the square one up and to the right of the selected pawn to the solution array
                     solution += [row - 1, column + 1]
         
-        #add to the solution variable all the possible moves if the piece is a black pawn
-        if(piece == "bP"):
-            if(row < 7 and self.piece_at_coordinates(self, row + 1, column)[0] != "b"):
+        #this adds to the solution variable all the possible moves if the piece is a black pawn
+        #this basically works the same as the previous collection of if statements
+        #the only difference is that the - signs become + signs since the pawns are moving down the board not up it
+        #also instead of checking if the pawn is on a square greater than 0 it will check if it is on one less than 7
+        if(piece == "bP"): #checks to see if piece is black pawn
+
+            #checks to see if pawn is not on the last row and if so, if the square in front of the pawn is empty
+            if(row < 7 and self.piece_at_coordinates(self, row + 1, column) == "--"):
+
+                #adds the square in front of the pawn to the solution array of possible moves
                 solution += [row + 1, column]
-                if(row == 1 and self.piece_at_coordinates(self, row + 2, column)[0] != "b"):
+
+                #adds the extra check to see if the pawn is on it's starting row and if so, if it can move 2 forward
+                #it checks to see if the square 2 in front of the pawn is empty
+                if(row == 1 and self.piece_at_coordinates(self, row + 2, column)[0] == "--"): 
+
+                    #adds the option to move 2 forward to the solution array
                     solution += [row + 2, column]
-            if(row < 7 and (column > 0 and column < 7)):
+
+            #this adds the possibility of the pawn moving diagnally to take other pieces
+            if(row < 7 and (column > 0 and column < 7)): #checks that the pawn is not on the last row and not on the -->
+                #leftmost or rightmost columns
+
+                #checks if the piece one below and one to the left of the pawn is white
                 if(self.piece_at_coordinates(self, row + 1, column - 1)[0] == "w"):
+
+                    #if so it adds that square (one down and one to the left) to the solution array of possible moves
                     solution += [row + 1, column - 1]
+
+                #checks if the piece one below and one to the right of the pawn is white
                 if(self.piece_at_coordinates(self, row + 1, column + 1)[0] == "w"):
+                    
+                    #if so it adds that square (one below and one to the right) to the solution array of possible moves
                     solution += [row + 1, column + 1]
 
         #add to the solution variable all the possible moves if the piece is a rook
