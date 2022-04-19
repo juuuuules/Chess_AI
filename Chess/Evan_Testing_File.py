687a50d9e895edae862afa9f963843f1b4adf0fe
Evan Banerjee <ebanerjee22@gmail.com>
2022-04-19 09:44:04 -0400

    don't worry about it

--- Chess/Evan_Testing_File.py
+++ Chess/Evan_Testing_File.py
@@ -9,22 +9,13 @@ screen = pygame.display.set_mode((500, 500))
 screen.fill((255, 255, 255))
 
 while True:
-
-    screen.fill((255, 255, 0))
-
-    n = 0
-
-    for file in os.scandir("../images"):
-        if file.is_file():
-            image = pygame.image.load(file.path)
-
-            screen.blit(image, (n, n))
-        n += 40
-
     for event in pygame.event.get():
-
         if event.type == pygame.QUIT:
-            pygame.quit()
             quit()
+    image = pygame.image.load("../images/bB.png")
+    image = pygame.transform.scale(image, (500, 500))
+
+    screen.blit(image, (0, 0))
 
     pygame.display.update()
+
