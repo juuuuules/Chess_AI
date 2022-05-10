4bbaa5d16cfdb51ffb04e6df465fae93a83a053e
Julien Berman <jberman22@gds.org>
2022-05-10 11:09:21 -0500

    commit

--- Chess/chess_evaluator.py
+++ Chess/chess_evaluator.py
@@ -4,7 +4,9 @@ import random
 piece_score = {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "P": 1}
 CHECKMATE = 10000
 STALEMATE = 0
-#if position has a positive value, it's winning for white. Negative value if it's winning for black.
+
+MAX_DEPTH = 2
+
 
 #random move chess engine
 def find_random_move(valid_moves):
@@ -41,9 +43,21 @@ def find_random_move(valid_moves):
 
 
 #evaluation function that scores the board. This version is based only on material
-def evaluate(board):
+def evaluate(game_state):
+    #Checkmate handler
+    if game_state.is_checkmate:
+        if game_state.is_white_turn:
+            return -CHECKMATE
+        else:
+            return CHECKMATE
+
+    #Stalemate handler
+    elif game_state.is_stalemate:
+        return STALEMATE
+
+
     score  = 0
-    for row in board:
+    for row in game_state.board:
         for square in row:
             if square[0] == 'w':
                 score += piece_score[square[1]]     #adds value of piece at given square if the piece is white
@@ -52,34 +66,52 @@ def evaluate(board):
     return score
 
 
-#return a tuple (score, best_move) for the position at a given depth
-def minimax(game_state, depth):
-    if depth == 0 or game_state.is_checkmate or game_state.is_stalemate:
-        return (evaluate(game_state.board), None)
+#HELPER METHOD TO MAKE FIRST RECURSIVE CALL
+def minimax(game_state, valid_moves):
+    global best_move
+    best_move = None
+    minimax_algorithm(game_state, valid_moves, MAX_DEPTH, game_state.is_white_turn)
+    return best_move
+
+
+#return a score for the position at a given depth
+def minimax_algorithm(game_state, valid_moves, depth, is_white_turn):
+
+    if depth == 0:
+        return evaluate(game_state)
+    
+    if is_white_turn:
+        max_score = -CHECKMATE
+
+        for move in valid_moves:
+            game_state.make_move(move)
+            next_moves = game_state.get_valid_moves()
+
+            score = minimax_algorithm(game_state, next_moves, depth - 1, not is_white_turn)
+
+            if score > max_score:
+                max_score = score
+                if depth == MAX_DEPTH:
+                    best_move = move
+
+            game_state.undo_move()
+
+        return max_score
+
     else:
-        if game_state.is_white_turn: #if white to move
-            best_score = -CHECKMATE
-            best_move = None
+        min_score = CHECKMATE
 
-            for move in game_state.valid_moves():
-                new_game_state = game_state.make_move(move)
-                score, move = minimax(new_game_state, depth - 1)
+        for move in valid_moves:
+            game_state.make_move(move)
+            next_moves = game_state.get_valid_moves()
 
-                if score > best_score:  #if white maximizes score
-                    best_score = score
+            score = minimax_algorithm(game_state, next_moves, depth - 1, not is_white_turn)
+
+            if score < min_score:
+                min_score = score
+                if depth == MAX_DEPTH:
                     best_move = move
-            return (best_score, best_move)
-        else:   #if black to move
-            best_score = CHECKMATE
-            best_move = None
-            for move in game_state.valid_moves:
-                new_game_state = game_state.make_move(move)
-                score, move = minimax(new_game_state, depth - 1)
-                
-                if score < best_score:  #if black minimizes score
-                    best_score = score
-                    best_move = move
-            return (best_score, best_move)
-
-
+            game_state.undo_move()
+        return min_score
+        
 
