e441aba7eeac9627dd522b9f2cbf2525af732ad0
Julien Berman <jberman22@gds.org>
2022-05-12 19:52:47 -0400

    commit

--- Chess/chess_evaluator.py
+++ Chess/chess_evaluator.py
@@ -1,47 +1,29 @@
+
+
 from lib2to3.refactor import get_all_fix_names
 import random
 
+
+"""
+Piece scores. Eventually we will have a weighted array.
+"""
 piece_score = {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "P": 1}
+
+
+
+"""
+Global variables.
+"""
 CHECKMATE = 10000
 STALEMATE = 0
-
 MAX_DEPTH = 2
 
 
-#random move chess engine
-def find_random_move(valid_moves):
-    num = random.randint(0, len(valid_moves) - 1) #randint is inclusive of both parameters
-    return valid_moves[num]
-
-# def minimax(game_state, valid_moves):
-#     min_score = -CHECKMATE
-#     best_move = None
-    
-#     turn_multiplier = 1 if game_state.is_white_turn else -1
-
-#     for player_move in valid_moves:             #player_move is move of the player who's turn it is; opponent is the other player
-#         game_state.make_move(player_move)
-        
-#         if game_state.is_checkmate:
-#             score = CHECKMATE
-
-#         elif game_state.is_stalemate:
-#             score = STALEMATE
-#         else: 
-#             score = turn_multiplier * evaluate(game_state.board)
-
-#         if score > min_score:           #if the score is better than the min, then make THAT equal to the min. For both colors, the goal is to get the max score.
-#             min_score = score
-#             best_move = player_move
-        
-#         game_state.undo_move()
-
-#     return best_move
 
 
-
-
-
+"""
+Evaluation Function.
+"""
 #evaluation function that scores the board. This version is based only on material
 def evaluate(game_state):
     #Checkmate handler
@@ -64,14 +46,20 @@ def evaluate(game_state):
             if square[1] == 'b':
                 score -= piece_score[square[1]]     #subtracts value of piece at given quare if the piece is black
     return score
+        
 
+"""
+Best move functions.
+"""
 
-#HELPER METHOD TO MAKE FIRST RECURSIVE CALL
+#Minimax Algorithm
+
+#Helper method. Makes first minimax call.
 def minimax(game_state, valid_moves):
     return minimax_algorithm(game_state, valid_moves, MAX_DEPTH, game_state.is_white_turn)[1]
 
 
-#return a score for the position at a given depth
+#return a score, best move at a given depth
 def minimax_algorithm(game_state, valid_moves, depth, is_white_turn):
     best_move = None
     if depth == 0:
@@ -111,4 +99,10 @@ def minimax_algorithm(game_state, valid_moves, depth, is_white_turn):
             game_state.undo_move()
         return min_score, best_move
         
+
+
+#Function that makes a random move
+def find_random_move(valid_moves):
+    num = random.randint(0, len(valid_moves) - 1) #randint is inclusive of both parameters
+    return valid_moves[num]
 
