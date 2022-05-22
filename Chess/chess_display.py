e51f6f850fca4d2404372423fda0c9c91a59b8e5
Evan Banerjee <ebanerjee22@gmail.com>
2022-05-22 13:55:21 -0400

    accidently wrote deep blue. now it's shallow blue

--- Chess/chess_display.py
+++ Chess/chess_display.py
@@ -130,21 +130,21 @@ def main():
         font = p.font.SysFont("Helvetica", 20)
         font2 = p.font.SysFont("Helvetica", 70)
 
-        deep_blue_text = font2.render("DEEP BLUE", True, (0, 0, 0))
+        shallow_blue_text = font2.render("SHALLOW BLUE", True, (0, 0, 0))
         sandbox_text = font.render("sandbox", True, (0, 0, 0))
         against_computer_text = font.render("play against computer", True, (0, 0, 0))
 
-        deep_blue_text_rect = deep_blue_text.get_rect()
+        shallow_blue_text_rect = shallow_blue_text.get_rect()
         sandbox_text_rect = sandbox_text.get_rect()
         against_computer_text_rect = sandbox_text.get_rect()
 
-        deep_blue_text_rect.center = (340, 70)
+        shallow_blue_text_rect.center = (340, 70)
         sandbox_text_rect.center = (150, 170)
         against_computer_text_rect.center = (480, 170)
 
-        p.draw.rect(screen, (0, 255, 255), deep_blue_text_rect)
+        p.draw.rect(screen, (0, 255, 255), shallow_blue_text_rect)
 
-        screen.blit(deep_blue_text, deep_blue_text_rect)
+        screen.blit(shallow_blue_text, shallow_blue_text_rect)
         screen.blit(sandbox_text, sandbox_text_rect)
         screen.blit(against_computer_text, against_computer_text_rect)
 
